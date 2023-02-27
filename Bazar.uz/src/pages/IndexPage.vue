a8c39b3e06c49b9139e5efc485e8955553e630d3
SanjarbekUsmonov <noobpusk@gmail.com>
2023-02-28 01:13:02 +0500

    first commit

---
+++ Bazar.uz/src/pages/IndexPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <section>
+          <div class="container">
+              <carusel/>
+          </div>
+      </section>
+  </div>
+</template>
+
+<script>
+import carusel from '../components/carusel.vue'
+  export default {
+  components: { carusel },
+    name:"IndexPage",
+    data() {
+      return {
+
+      }
+    },
+  }
+</script>
+
+<style lang="css" scoped>
+.container{
+  width: 90%;
+  height: 200vh;
+  background: rgb(207, 205, 205);
+  margin: 0 auto;
+}
+</style>
