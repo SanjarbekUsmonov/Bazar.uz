858d5293810e783a21ea81d43edd7f91e4752d9b
SanjarbekUsmonov <noobpusk@gmail.com>
2023-03-01 02:19:00 +0500

    cardaga text qo'shildi

--- Bazar.uz/src/components/card.vue
+++ Bazar.uz/src/components/card.vue
@@ -1,50 +1,52 @@
 <template>
-  <div class="q-pa-md q-gutter-md row justify-between">
-    <q-card class="my-card" v-for="card in 8" :key="card">
-      <q-img style="height: 250px;" src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+  <div>
+    <div class=" bg-green">
+      <h3>Hello Bazar</h3>
+    </div>
+    <div class="q-pa-md q-gutter-md row justify-between">
+      <q-card class="my-card" v-for="card in 8" :key="card">
+        <q-img
+          style="height: 250px"
+          src="https://cdn.quasar.dev/img/chicken-salad.jpg"
+        />
 
-      <q-card-section>
-        <div class="row no-wrap items-center">
-          <div class="col text-h6 ellipsis">
-            Cafe Basilico
+        <q-card-section>
+          <div class="row no-wrap items-center">
+            <div class="col text-h6 ellipsis">Cafe Basilico</div>
           </div>
-        </div>
 
-        <q-rating v-model="stars" :max="5" size="22px" />
-      </q-card-section>
+          <q-rating v-model="stars" :max="5" size="22px" />
+        </q-card-section>
 
-      <q-card-section class="q-pt-none">
-        <div class="text-subtitle1">
-          $・Italian, Cafe
+        <q-card-section class="q-pt-none">
+          <div class="text-subtitle1">$・Italian, Cafe</div>
+          <div class="text-caption text-grey">
+            Small plates, salads & sandwiches in an intimate setting.
+          </div>
+        </q-card-section>
+        <div>
+          <q-btn flat round icon="event" />
+          <q-btn flat color="primary"> Reserve </q-btn>
         </div>
-        <div class="text-caption text-grey">
-          Small plates, salads & sandwiches in an intimate setting.
-        </div>
-      </q-card-section>
-      <div>
-        <q-btn flat round icon="event" />
-       <q-btn flat color="primary">
-          Reserve
-        </q-btn>
-      </div>
-    </q-card>
+      </q-card>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 export default {
-  setup () {
+  setup() {
     return {
-      stars: ref(4)
-    }
-  }
-}
+      stars: ref(4),
+    };
+  },
+};
 </script>
 
 <style lang="css" scoped>
-.my-card{
+.my-card {
   width: 100%;
   height: 460px;
   max-width: 250px;
