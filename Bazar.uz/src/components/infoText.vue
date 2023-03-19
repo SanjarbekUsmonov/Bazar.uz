3363f4e5ec6b4a2298ec60e8bbaf49b91aa9cc36
SanjarbekUsmonov <noobpusk@gmail.com>
2023-03-19 15:04:30 +0500

    infoText

---
+++ Bazar.uz/src/components/infoText.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="info-text text-h5 text-weight-medium">
+      Palov qozoni Kukmara shisha va shisha bo'lmagan qopqoqli, granit Ultra (moviy) liniyasi
+    </div>
+    <div class="info-data q-mt-md">
+        <div>
+          <span >Sotuvchi:</span> <span class="q-ml-xl">kukmara</span>
+        </div>
+        <div>
+          <span>Yetkazib berish:</span> <span>1 kun, bepul</span>
+        </div>
+    </div>
+    <q-separator class="q-mt-md"/>
+    <div class="info-amount">
+        <div>Miqdor:</div>
+        <div class="info-amount-active">
+
+        </div>
+    </div>
+    <div class="info-btn">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
