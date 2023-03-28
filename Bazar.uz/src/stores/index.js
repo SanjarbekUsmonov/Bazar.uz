e25d2f266533f2d56733b9a9ffdf23ff19871513
SanjarbekUsmonov <noobpusk@gmail.com>
2023-03-29 00:09:57 +0500

    pinia

--- Bazar.uz/src/stores/index.js
+++ Bazar.uz/src/stores/index.js
@@ -1,14 +1,26 @@
-import { store } from 'quasar/wrappers'
-import { createPinia } from 'pinia'
+import { defineStore, createPinia } from 'pinia';
+import {store} from 'quasar/wrappers'
+import axios from 'axios'
 
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
+
+export const useCounterStore = defineStore('store', {
+  state: () => ({
+    counter: 0,
+    category: []
+  }),
+  getters: {
+    doubleCount: (state) => state.counter * 2,
+  },
+  actions: {
+    async GETAPI() {
+      let apiInfo = await axios.get("http://bazarcom.pythonanywhere.com/category/")
+        this.category = apiInfo.data
+        console.log(this.category)
+    }
+  },
+});
+
+
 
 export default store((/* { ssrContext } */) => {
   const pinia = createPinia()
