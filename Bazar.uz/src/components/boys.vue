956a9885b68a95f45669e485b2171fbacf54e732
SanjarbekUsmonov <noobpusk@gmail.com>
2023-03-29 00:09:22 +0500

    props

--- Bazar.uz/src/components/boys.vue
+++ Bazar.uz/src/components/boys.vue
@@ -3,15 +3,16 @@
     <div >
       <h3 style="font-weight: 450">Erkaklar kiyimlari...</h3>
     </div>
-      <card/>
+      <card :products="store.category"/>
   </div>
 </template>
 
-<script>
+<script setup>
 import card from './card.vue';
-export default {
-  components: { card },
-};
+import {useCounterStore} from "src/stores/index"
+const store = useCounterStore()
+
+
 </script>
 
 <style lang="css" scoped>
