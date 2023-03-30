8c36d7553b0dd70634495239d695a887800a9bd1
SanjarbekUsmonov <noobpusk@gmail.com>
2023-03-30 12:53:22 +0500

    props

---
+++ Bazar.uz/src/components/childern.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class=" ">
+      <h3 style="font-weight: 450;">Bolalar kiyimi...</h3>
+    </div>
+    <card :products="store.childern"/>
+  </div>
+</template>
+
+<script setup>
+import card from './card.vue';
+import {useCounterStore} from "src/stores/index"
+const store = useCounterStore()
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
